20597dc8c2e359ea99578f13c85dd95636f6dc63
Marco Bonati <marco.bonati@vipera.com>
2018-06-28 16:52:18 +0200

    Added new modules and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WebAdminModulesProvider } from './web-admin-modules-provider.module';
+import { RouterModule, Routes } from '@angular/router'
+import { WebAdminConsoleComponent, AuthGuard } from 'web-console-core'
+import { WebConsoleLoginComponent } from 'web-console-login'
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: WebConsoleLoginComponent },
+  { path: 'dashboard', component: WebAdminConsoleComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, WebAdminModulesProvider
+    BrowserModule,  
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  , WebAdminModulesProvider
   ],
-  providers: [],
+  providers: [ WebAdminModulesProvider ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/web-admin-modules-provider.module.ts
+++ src/app/web-admin-modules-provider.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core'
-import {WebAdminCoreModule} from 'web-console-core'
+import { WebAdminCoreModule } from 'web-console-core'
+import { WebConsoleLoginModule } from 'web-console-login'
+
 @NgModule({
-    imports:[WebAdminCoreModule],
-    exports:[WebAdminCoreModule]
+    imports:[ WebAdminCoreModule.withConfig({loginRoute:"login",dashboardRoute:"dashboard"}), WebConsoleLoginModule],
+    exports:[ WebAdminCoreModule, WebConsoleLoginModule ]
 })
 export class WebAdminModulesProvider {
-
+    
 }
